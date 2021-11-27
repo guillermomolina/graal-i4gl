50fd4dcd01a4e99618febfa05944d2f54ddf0820
Guillermo A. Molina <guillermoadrianmolina@hotmail.com>
2021-11-27 19:02:51 +0000

    Unpatch I4GLDisplayNode

--- language/src/main/java/com/guillermomolina/i4gl/nodes/statement/I4GLDisplayNode.java
+++ language/src/main/java/com/guillermomolina/i4gl/nodes/statement/I4GLDisplayNode.java
@@ -3,13 +3,9 @@ package com.guillermomolina.i4gl.nodes.statement;
 import com.guillermomolina.i4gl.nodes.I4GLTypeSystem;
 import com.guillermomolina.i4gl.nodes.expression.I4GLExpressionNode;
 import com.guillermomolina.i4gl.runtime.context.I4GLContext;
-import com.guillermomolina.i4gl.runtime.context.I4GLLanguageView;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.dsl.TypeSystemReference;
-import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.NodeInfo;
 
 @TypeSystemReference(I4GLTypeSystem.class)
@@ -18,11 +14,14 @@ import com.oracle.truffle.api.nodes.NodeInfo;
 public abstract class I4GLDisplayNode extends I4GLStatementNode {
 
     @Specialization
-    @TruffleBoundary
-    public Object println(Object value,
-                    @CachedLibrary(limit = "3") InteropLibrary interop) {
-        I4GLContext.get(this).getOutput().println(interop.toDisplayString(I4GLLanguageView.forValue(value)));
-        return value;
+    public void display(String argument) {
+        I4GLContext.get(this).getOutput().println(argument);
     }
 
+    @Specialization
+    public void display(Object argument) {
+        if(argument != null) {
+            I4GLContext.get(this).getOutput().println(argument.toString());
+        }
+    }
 }
